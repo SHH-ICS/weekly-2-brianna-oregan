ea6243f6b3b904888e74352066ce6bb500409f39
brianna-oregan <brianna.oregan@stu.ocsb.ca>
2024-10-22 02:41:39 +0000

    add PHP and MDL

--- assignment.py
+++ assignment.py
@@ -1,1 +1,15 @@
+import math
 
+# get diameter from user
+diameter = float(input("Enter the diameter of your circle in centimeters: "))
+
+# calculate radius
+radius = diameter / 2
+
+# calculate area and circumference
+area = math.pi * (radius ** 2)
+circumference = 2 * math.pi * radius
+
+# display results
+print("The area of your circle is", area, "centimeters squared.")
+print("The circumference of your circle is", circumference, "centimeters.")
--- handler.php
+++ handler.php
@@ -1,23 +1,37 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <title>Welcome Page</title>
-  </head>
 
-  <body>
-    
-    <?php
-    $myVariable = "";
-    if ( isset( $_POST['myVariable'] ) ){
-      $myVariable = $_POST['myVariable'];
-    }
-    echo "<h1>My Program</h1>\n";
-    echo "<p>My Variable is = ".$myVariable."</p>\n";
-    ?>
-    
-  </body>
-  
+<head>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <title>Circle Calculator Results</title>
+</head>
+
+<body>
+
+  <?php
+  $diameter = 0;
+  $area = 0;
+  $circumference = 0;
+
+  if (isset($_POST['diameter'])) {
+    $diameter = (float)$_POST['diameter'];
+
+    // calculate radius area and circumference
+    $radius = $diameter / 2;
+    $area = pi() * ($radius ** 2);
+    $circumference = 2 * pi() * $radius;
+  }
+  ?>
+
+  <p>The area of your circle is: <?php echo number_format($area, 2); ?></p>
+  <p>The circumference of your circle is: <?php echo number_format($circumference, 2); ?></p>
+
+  <a href="index.html" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+    Start Over
+  </a>
+
+</body>
+
 </html>
